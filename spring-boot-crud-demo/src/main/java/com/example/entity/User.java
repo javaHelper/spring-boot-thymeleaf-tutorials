be7d304ce00c8bc462013301ce2708bb9518eeaa
Prateek <54174687+javaHelper@users.noreply.github.com>
2023-11-21 09:56:30 +0530

    Add files via upload

---
+++ spring-boot-crud-demo/src/main/java/com/example/entity/User.java
@@ -0,0 +1,39 @@
+package com.example.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false, unique = true, length = 45)
+    private String email;
+
+    @Column(length = 15, nullable = false)
+    private String password;
+
+    @Column(length = 45, nullable = false, name = "first_name")
+    private String firstName;
+
+    @Column(length = 45, nullable = false, name = "last_name")
+    private String lastName;
+
+    private boolean enabled;
+}
---
+++ spring-boot-crud-demo/src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO usersdb.users (id, email, enabled, first_name, last_name, password) VALUES(1, 'john.doe@gmail.com', 1, 'John', 'Doe', '12345');
+INSERT INTO usersdb.users (id, email, enabled, first_name, last_name, password) VALUES(2, 'mike.doe@test.com', 1, 'Mike', 'Doe', '12345');
+INSERT INTO usersdb.users (id, email, enabled, first_name, last_name, password) VALUES(3, 'jane.doe@test.com', 1, 'Jane', 'Doe', '12345');
+INSERT INTO usersdb.users (id, email, enabled, first_name, last_name, password) VALUES(4, 'jack.doe@test.com', 0, 'Jack', 'Doe', '12345');
